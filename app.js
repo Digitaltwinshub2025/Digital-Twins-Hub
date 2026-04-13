9fe525bca9e5770070d8f92d53628da06f3b4b5b
digitaltwinshub <253614467+Digitaltwinshub2025@users.noreply.github.com>
2026-04-13 15:26:43 -0700

    feat: use learning hub hero as page background

--- app.js
+++ app.js
@@ -4342,7 +4342,7 @@
       : `
         <div class="space-y-10 relative">
           <div class="pointer-events-none fixed inset-0 -z-10">
-            <div class="absolute inset-0 bg-center bg-cover" style="background-image:url('assets/Learning%20Hub/Slideshow/1.png');"></div>
+            <div class="absolute inset-0 bg-center bg-cover" style="background-image:url('assets/Learning%20Hub/learning-hub-hero.jpg');"></div>
             <div class="absolute inset-0 bg-black/55"></div>
             <div class="absolute inset-0 bg-gradient-to-b from-black/30 via-black/55 to-black/80"></div>
           </div>
